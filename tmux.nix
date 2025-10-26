0c59f7918f4adbd1de71da37e3530cfe26a90839
anoromi <and.zagorulko@gmail.com>
2025-10-26 15:31:32 +0200

    improve tmux vim vim mode

--- tmux.nix
+++ tmux.nix
@@ -71,6 +71,40 @@ set -g @catppuccin_flavour 'latte'
 			set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
 			set-environment -g COLORTERM \"truecolor\"
 
+			set-window-option -g mode-keys vi
+      unbind -T copy-mode-vi h
+      unbind -T copy-mode-vi j
+      unbind -T copy-mode-vi k
+      unbind -T copy-mode-vi l
+
+      bind -T copy-mode-vi h send -X cursor-left
+      bind -T copy-mode-vi s send -X cursor-right
+      bind -T copy-mode-vi n send -X cursor-up
+      bind -T copy-mode-vi t send -X cursor-down
+
+      bind -T copy-mode-vi - send -X end-of-line
+      bind -T copy-mode-vi _ send -X start-of-line
+
+      bind -T copy-mode-vi N \\
+        send -X cursor-up \\; send -X cursor-up \\; send -X cursor-up \\; send -X cursor-up \\; \\
+        send -X cursor-up \\; send -X cursor-up \\; send -X cursor-up \\; send -X cursor-up
+
+      bind -T copy-mode-vi T \\
+        send -X cursor-down \\; send -X cursor-down \\; send -X cursor-down \\; send -X cursor-down \\; \\
+        send -X cursor-down \\; send -X cursor-down \\; send -X cursor-down \\; send -X cursor-down
+
+      # Setup 'v' to begin selection, just like Vim
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      
+      # Setup 'y' to yank (copy), just like Vim
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      
+      # Update the default binding of 'Enter' to use copy-selection-and-cancel
+      unbind-key -T copy-mode-vi Enter
+      bind-key -T copy-mode-vi Enter send-keys -X copy-selection-and-cancel
+
+      bind -T copy-mode-vi l send -X search-again
+      bind -T copy-mode-vi L send -X search-reverse   # previous match (optional)
     ";
     # plugins = with pkgs;
     #   [
